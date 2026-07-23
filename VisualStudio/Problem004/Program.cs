799bad6cac4e8965c7ae571113e516eae5f19b39
Darren <db.darren6546@gmail.com>
2026-07-23 15:43:50 +0100

    Logic Learning
    Learning the basic logic and implementation of the 'isPalindrome' function.

--- VisualStudio/Problem004/Program.cs
+++ VisualStudio/Problem004/Program.cs
@@ -1,11 +1,26 @@
 ﻿
+// Largest Palindrome Product
 
-Problem4.Solve();
+/*
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+ * 
+ * Fine the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ */
+
+Problem4.Solve(2);
 
 public static class Problem4
 {
-    public static void Solve()
+    public static void Solve(int digits)
     {
-        Console.WriteLine("This is where the solution goes for Problem4.");
+        //Creates string with (digit) length of all 9's.
+        int targetStr = int.Parse(new string('9', digits));
+        int MaxProd = targetStr * targetStr;
+        Console.Write($"Max Prod of {targetStr} * {targetStr} => {MaxProd}");
     }
+
+    private static bool isPalindrome(string var) => 
+        var.Equals(new String(var.Reverse().ToArray()), StringComparison.OrdinalIgnoreCase);
+    
 }
