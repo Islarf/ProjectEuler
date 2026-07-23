fc8ead94698d13866c84959745b3aa4da9b09369
Darren <db.darren6546@gmail.com>
2026-07-23 15:09:41 +0100

    Problem 4
    Created problem 4 and its class

---
+++ VisualStudio/Problem004/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Problem4.Solve();
+
+public static class Problem4
+{
+    public static void Solve()
+    {
+        Console.WriteLine("This is where the solution goes for Problem4.");
+    }
+}
