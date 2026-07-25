af8eae5c130288703c9f377c02f178e3ceb07bbf
Darren <db.darren6546@gmail.com>
2026-07-25 18:50:13 +0100

    First attempt
    After a lot of smashing my head against a wall, i got this for problem 9, but it's very much wrong and infinite looping.

---
+++ VisualStudio/Problem009/Program.cs
@@ -0,0 +1,45 @@
+﻿//Special Pythagorean Triplet
+
+/*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which:
+ * ---------- a^2 + b^2 = c^2
+ * 
+ * for example: 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+ * 
+ * There exists ONE pythagorean triple for which a + b + c = 1000.
+ * Find the product of abc.
+ */
+using EulerUtils;
+
+Console.WriteLine(Problem9.SolveLoop(1000));
+
+
+public class Problem9
+{
+    public static string SolveLoop(int cap = 12)
+    {
+        int a = 1; int b = 2;
+        while (true)
+        {
+            int c = (int)Math.Sqrt(a * a + b * b);
+            //Console.WriteLine($"{a} + {b} + {c} = {cap} // {a} * {b} * {c} = {a * b * c}");
+            if(a < b && b < c && a + b + c == cap)
+            {
+                return $"{a} + {b} + {c} = {cap} // {a} * {b} * {c} = {a*b*c}";
+            }
+            if(b >= cap / a)
+            {
+                a++;
+                b = a+1;
+            }
+            else
+            {
+                b++;
+            }
+        }
+        //return "None Found";
+    }
+}
+
+
+
+// ²
