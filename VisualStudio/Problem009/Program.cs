2634ec48996c84a817a01374659721e440c1771e
Darren <db.darren6546@gmail.com>
2026-07-25 19:01:17 +0100

    Finished!
    Added a none found exception, and a way out of the infinite loop (just incase) for other variables.

--- VisualStudio/Problem009/Program.cs
+++ VisualStudio/Problem009/Program.cs
@@ -19,7 +19,7 @@ public class Problem9
     public static string SolveLoop(int cap = 12)
     {
         int a = 1; int b = 2;
-        while (a < cap-1)
+        while (a < cap)
         {
             int c = (int)Math.Sqrt(a * a + b * b);
             //Console.WriteLine($"{a} + {b} + {c} = {cap} // {a} * {b} * {c} = {a * b * c}");
@@ -27,7 +27,7 @@ public class Problem9
             {
                 return $"{a} + {b} + {c} = {cap} // {a} * {b} * {c} = {a * b * c}";
             }
-            if(a+b+c > cap)
+            if(a+b+c > cap || b >= cap)
             {
                 a++;
                 b = a+1;
@@ -37,7 +37,7 @@ public class Problem9
                 b++;
             }
         }
-        //return "None Found";
+        return "None Found";
     }
 }
 
