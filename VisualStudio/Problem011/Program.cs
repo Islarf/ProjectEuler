b16721acbee4be850726d98164e669fc47fede8b
Darren <db.darren6546@gmail.com>
2026-07-29 15:33:19 +0100

    Project 11
    Preparation to investigate Project 11

---
+++ VisualStudio/Problem011/Program.cs
@@ -0,0 +1,19 @@
+﻿//Largest Product in a Grid
+
+/*
+ * In the 20 x 20 grid below, four numbers along a diagonal line have been marked in red.
+ * The product of these numbers is 26 x 63 x 78 x 14 = 1788696.
+ * What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally)
+ */
+
+
+public class Problem11
+{
+    public static int SolveLoop(int adj = 4)
+    {
+        int solution = 1;
+
+
+        return solution;
+    }
+}
